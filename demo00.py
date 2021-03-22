b1de46f51ecb4757459a3712e42bbe3c011611aa
Michael Nikitenko <xm4dn355x@gmail.com>
2021-03-22 12:04:54 +0500

    testing first print with colorize

---
+++ demo00.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+########################################################################
+#                                                                      #
+# First demo script for testing rich library                           #
+#                                                                      #
+# MIT License                                                          #
+# Copyright (c) 2021 Michael Nikitenko                                 #
+#                                                                      #
+########################################################################
+
+
+from rich import print
+
+
+if __name__ == '__main__':
+    print('[black on white]Hello World![/black on white]')
