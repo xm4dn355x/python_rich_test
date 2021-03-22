1556a38610f811d43b3d298be8422daa056fb7db
Michael Nikitenko <xm4dn355x@gmail.com>
2021-03-22 13:23:50 +0500

    testing f-strings and colorizing formatting

--- demo00.py
+++ demo00.py
@@ -14,3 +14,8 @@ from rich import print
 
 if __name__ == '__main__':
     print('[black on white]Hello World![/black on white]')
+    print("[bold yellow on red blink]Блинк в винде не работает")
+    print("[bold yellow on red]Зато ярко понятно читается")
+    warn_b = "[bold yellow on red]"
+    warn_e = "[/bold yellow on red]"
+    print(f'{warn_b}Тестируем F-строки{warn_e} Не закрашенный')  # Работает!
